8d83da461dccef559b8fda0e2d2731c52fe29c14
josh <48034855+JDJoffe@users.noreply.github.com>
2019-11-03 22:08:45 +1100

    graph stuff

--- Assets/Scripts/CustomHeart.cs
+++ Assets/Scripts/CustomHeart.cs
@@ -48,7 +48,13 @@ public class CustomHeart : MonoBehaviour
     int currentIndex = 0;
 
 
-
+    public enum CurveType
+    {
+        Curve1,
+        Curve2
+    }
+    public CurveType curveType;
+    Curve curve;
     void Start()
     {
         Init();
@@ -95,6 +101,15 @@ public class CustomHeart : MonoBehaviour
         targetVertex = mVertices[selectedIndices[currentIndex]];
         starttime = Time.time;
         isAnimate = true;
+        // check the curvetype enum it is set to in the inspector and perform that curve
+        if (curveType == CurveType.Curve1)
+        {
+            CurveType1();
+        }
+        else if (curveType == CurveType.Curve2)
+        {
+            CurveType2();
+        }
     }
 
     void FixedUpdate()
@@ -143,7 +158,14 @@ public class CustomHeart : MonoBehaviour
             vert = mVertices[i];
 
             float distance = Mathf.Sqrt(sqrMagnitude);
-
+            // get curve pos by the distance and multiply y by force to get
+            // the value it will increment by
+            // v3 to store increment info and apply the transform to mvertices[i]
+            float increment = curve.GetPoint(distance).y * force; 
+            Vector3 translate = (vert * increment) * Time.deltaTime; 
+            Quaternion rotation = Quaternion.Euler(translate);
+            Matrix4x4 m = Matrix4x4.TRS(translate, rotation, Vector3.one);
+            mVertices[i] = m.MultiplyPoint3x4(mVertices[i]);
         }
         oMesh.vertices = mVertices;
         oMesh.RecalculateNormals();
@@ -158,10 +180,41 @@ public class CustomHeart : MonoBehaviour
 
     void CurveType1()
     {
+        //  \ (0,1,0)   
+        //   \
+        //    \ (.5,.5,0)
+        //     \
+        //      \ (1,0,0)
+        // negative linear regression 
+        // curve points are coords the line goes to.
+        // this line will go diagonally down 
+        Vector3[] curvePoints = new Vector3[3];
+        // start point at top left
+        curvePoints[0] = new Vector3(0, 1, 0);
+        // mid point at the middle of graph
+        curvePoints[1] = new Vector3(.5f, .5f, 0);
+        // end  point at bottom right
+        curvePoints[2] = new Vector3(1, 0, 0);
+        // tdraw false, previous drawn curve can be seen it tdraw is true
+        curve = new Curve(curvePoints[0], curvePoints[1], curvePoints[2], false);
     }
 
     void CurveType2()
     {
+        //      _ (.5,1,0)
+        //     / \
+        //    /   \
+        //   /     \
+        //  / zero  \ (1,0,0)
+        // concave down parabola 
+        Vector3[] curvepoints = new Vector3[3];
+        // start point on bottom left
+        curvepoints[0] = new Vector3(0, 0, 0);
+        // mid point at top middle
+        curvepoints[1] = new Vector3(0.5f, 1, 0);
+        // end point at bottom right
+        curvepoints[2] = new Vector3(1, 0, 0);
+        curve = new Curve(curvepoints[0], curvepoints[1], curvepoints[2], false); 
     }
 
     public void ShowNormals()
